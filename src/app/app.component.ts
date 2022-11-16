6fbfe5ba08cecebc1c3163f40b9f9a0510d76287
Katha <katharinameinhardt94@gmail.com>
2022-11-16 23:20:48 +0100

    setup data binding form <=> template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ReplacementDefinition} from "./replacement-definition";
 import {FormArray, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'tag-replacer';
-  replacementDefinitions: ReplacementDefinition[] = [
+
+  tableData: ReplacementDefinition[] = [
     {enabled: true, tag: 'foo', replacement: 'bar'},
     {enabled: true, tag: 'escaping', replacement: 'ERROR'},
     {enabled: true, tag: 'tag', replacement: '{tag}'},
@@ -18,12 +20,19 @@ export class AppComponent {
 
   columnsToDisplay = ['enabled', 'tag', 'replacement'];
 
-  formGroup = new FormGroup({
+  form = new FormGroup({
     inputText: new FormControl(),
-    replacementDefinitions: new FormArray([]),
+    replacementDefinitions: new FormArray(this.tableData.map(val => new FormGroup({
+      enabled: new FormControl(val.enabled),
+      tag: new FormControl(val.tag),
+      replacement: new FormControl(val.replacement),
+    }))),
     outputText: new FormControl(),
   });
 
   constructor() {}
 
+  pause() {
+    debugger;
+  }
 }
